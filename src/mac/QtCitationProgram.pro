85af924fbcbae797eb34e98b9031cc0158f3c560
emmprokak <89413115+emmprokak@users.noreply.github.com>
2021-11-26 13:03:14 +0200

    Add files via upload

---
+++ src/mac/QtCitationProgram.pro
@@ -0,0 +1,42 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+CONFIG += app_bundle
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    bookcitationwindow.cpp \
+    chaptercitationwindow.cpp \
+    citation.cpp \
+    infowindow.cpp \
+    main.cpp \
+    mainwindow.cpp \
+    papercitationwindow.cpp
+
+HEADERS += \
+    bookcitationwindow.h \
+    chaptercitationwindow.h \
+    citation.h \
+    infowindow.h \
+    mainwindow.h \
+    papercitationwindow.h
+
+FORMS += \
+    bookcitationwindow.ui \
+    chaptercitationwindow.ui \
+    infowindow.ui \
+    mainwindow.ui \
+    papercitationwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+RESOURCES += \
+    images.qrc
---
+++ src/mac/citation.cpp
@@ -0,0 +1,118 @@
+/**
+Qt Citation Program
+Copyright (C) 2021 Prokakis Emmanouil
+
+Qt Citation Program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qt Citation Program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qt Citation Program. If not, see <https://www.gnu.org/licenses/>.
+**/
+
+#include "citation.h"
+#include <QString>
+#include <QStringList>
+#include <QPushButton>
+
+Citation::Citation(){}
+
+QString& Citation::format_author(QString &name, QString &style)
+{
+    if(style == "Harvard Style")
+    {
+        if(name.contains(' '))
+        {
+            QStringList elements = name.split(' ');
+            name = elements[0][0].toUpper();
+            for(int i = 1; elements[0][i] != NULL; i++)
+                name += elements[0][i].toLower();
+            name += ' ' + elements[1][0].toUpper() + '.';
+        }
+        else
+        {
+            for(int i = 0; name[i] != NULL; i++)
+                name[i] = name[i].toUpper();
+        }
+    }
+
+    else if(style == "APA Style")
+    {
+        if(name.contains(' '))
+        {
+            QStringList elements = name.split(' ');
+            name = elements[0][0].toUpper();
+            for(int i = 1; elements[0][i] != NULL; i++)
+                name += elements[0][i].toLower();
+            name += ", ";
+            name += elements[1][0].toUpper() + '.';
+        }
+        else
+        {
+            for(int i = 0; name[i] != NULL; i++)
+                name[i] = name[i].toUpper();
+        }
+    }
+    return name;
+}
+QString& Citation::format_title(QString& title)
+{
+    int i = 0, words = 1;
+    //count words
+    while(title[i] != NULL)
+    {
+        if(title[i] == ' ')
+            ++words;
+        ++i;
+    }
+    //figure out capital letters
+    QStringList elements = title.split(' ');
+    for(int j = 0; j < words; j++)
+    {
+        if(elements[j] == "or" || (elements[j] == "the" && j != 0) ||
+                elements[j] == "of" || elements[j] == "and" ||
+                elements[j] == "in" || elements[j] == "at")
+            elements[j][0] = elements[j][0].toLower();
+        else
+            elements[j][0] = elements[j][0].toUpper();
+    }
+    //format finalized title
+    title = "";
+    for(int j = 0; j < words; j++)
+    {
+        title += elements[j];
+        //no space at the end of the title
+        if(j != words - 1)
+            title += " ";
+    }
+
+    return title;
+}
+
+void Citation::clear_layout(QLayout *layout)
+{
+    if (layout == NULL)
+        return;
+
+    QLayoutItem *item;
+    while((item = layout->takeAt(0)))
+    {
+    /**
+        if (item->layout())
+        {
+            clear_layout(item->layout());
+            delete item->layout();
+        }
+*/
+        if (item->widget())
+           delete item->widget();
+
+        delete item;
+    }
+}
